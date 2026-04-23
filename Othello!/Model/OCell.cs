7b7f4a6f01605144fdc9d2f300e5bff3aac0d2b6
Jackson Zemanski <jacksonzemanski@gmail.com>
2026-04-23 12:13:02 -0500

    Add project files.

---
+++ Othello!/Model/OCell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello_.Model
+{
+    enum CellState
+    {
+        Empty,
+        Black,
+        White
+    }
+    internal class OCell
+    {
+        private CellState State;
+        
+        public OCell()
+        {
+            State = CellState.Empty;
+        }
+
+        public CellState GetState()
+        {
+            return State;
+        }
+
+        public void Place(CellState state)
+        {
+            State = state;
+        }
+
+        public void Flip()
+        {
+            if (State == CellState.Black)
+                State = CellState.White;
+            else if (State == CellState.White)
+                State = CellState.Black;
+            else if (State == CellState.Empty)
+                throw new InvalidOperationException("Cannot flip an empty cell. BAD DEV:(");
+        }
+    }
+}
